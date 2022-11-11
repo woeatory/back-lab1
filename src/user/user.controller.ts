77aba00f748aff510cd9f9938aad7f73790fcc63
Yurii <79090254+woeatory@users.noreply.github.com>
2022-11-11 22:29:58 +0200

    Added exception example

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,18 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './createUserDto';
+import { ForbiddenException } from './exceptions/test.exception';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserContoller {
   constructor(private userService: UserService) {}
   @Post('create')
-  createUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.createUser(createUserDto.userName);
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    try {
+      return this.userService.createUser(createUserDto.userName);
+    } catch (error) {
+      throw new ForbiddenException();
+    }
   }
   @Get('list')
   getUserList() {
