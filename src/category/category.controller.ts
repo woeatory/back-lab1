a846e7ae81dddf99f4f3865fcbc59a6ec187adae
Yurii <79090254+woeatory@users.noreply.github.com>
2022-11-15 15:31:39 +0200

    Formated imports

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './createCategoryDto';
 
