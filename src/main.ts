f3d4ce2620583faec004d2a0027e50565a537a8e
Yurii <79090254+woeatory@users.noreply.github.com>
2022-11-16 11:54:15 +0200

    Added Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { env } from 'process';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(env.PORT || 8000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(env.PORT || 8100);
 }
 bootstrap();
