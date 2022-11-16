6ddc28d506962715f79871ca607fd4a341651609
Yurii <79090254+woeatory@users.noreply.github.com>
2022-11-16 16:33:19 +0200

    Added Prisma Module in imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoryModule } from './category/category.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { RecordModule } from './record/record.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, CategoryModule, RecordModule],
+  imports: [UserModule, CategoryModule, RecordModule, PrismaModule],
   controllers: [],
   providers: [],
 })
